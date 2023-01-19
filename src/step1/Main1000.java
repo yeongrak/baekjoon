99cc6a35d8e79e34f96a32038ef62b7db53fa453
yeongrak <dudfkr481@gmail.com>
2023-01-19 23:01:41 +0900

    Initial commit

---
+++ src/step1/Main1000.java
@@ -0,0 +1,14 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Main1000 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        if (A > 0 && B < 10) {
+            System.out.println(A + B);
+        }
+    }
+}
---
+++ src/step1/Main18108.java
@@ -0,0 +1,13 @@
+package step1;
+
+import java.util.Scanner;
+
+public class Main18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bul;
+        int a = 543;
+        bul = sc.nextInt();
+        System.out.println(bul - a);
+    }
+}
