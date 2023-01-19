166908f24eefbf49d3c9863ded87f77faa9c64f0
yeongrak <dudfkr481@gmail.com>
2023-01-19 23:19:32 +0900

    문제 설명 추가

--- src/step1/Main10869.java
+++ src/step1/Main10869.java
@@ -2,6 +2,15 @@ package step1;
 
 import java.util.Scanner;
 
+/*
+ * 문제
+ * -두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+ * 입력
+ * -두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+ * 출력
+ * -첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+ */
+
 public class Main10869 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
